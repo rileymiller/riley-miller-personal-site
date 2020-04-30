86a0352634515e909cccb15af905bd5077cdf206
Riley Miller <rmllr000@gmail.com>
2020-04-30 00:12:41 -0600

    init commit

---
+++ src/components/bio.tsx
@@ -0,0 +1,70 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
+import styled from "@emotion/styled"
+
+import { rhythm } from "../utils/typography"
+
+const Bio = () => {
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      avatar: file(absolutePath: { regex: "/fb-profile.png/" }) {
+        childImageSharp {
+          fixed(width: 70, height: 70) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      site {
+        siteMetadata {
+          author
+          social {
+            twitter
+          }
+        }
+      }
+    }
+  `)
+
+  const { author, social } = data.site.siteMetadata
+
+  const BioContainer = styled.div({
+    display: `flex`,
+    marginBottom: rhythm(2.5)
+  })
+
+
+  return (
+    <BioContainer>
+      <Image
+        fixed={data.avatar.childImageSharp.fixed}
+        alt={author.name}
+        style={{
+          marginRight: rhythm(1 / 2),
+          marginBottom: 0,
+          minWidth: 70,
+          borderRadius: `100%`,
+        }}
+        imgStyle={{
+          borderRadius: `50%`,
+        }}
+      />
+      <p>
+        Written by <strong>{author}</strong>
+        {` `}
+        <a href={`https://twitter.com/${social.twitter}`}>
+          You should follow him on Twitter
+        </a>
+      </p>
+    </BioContainer>
+  )
+}
+
+export default Bio
