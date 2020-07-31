20a6af5d7e5be1ce4bf49e5a0569b9442fa2bee4
Riley Miller <rmllr000@gmail.com>
2020-07-31 04:44:43 -0600

    moar better

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -59,21 +59,20 @@ const BlogPostTemplate = ({ data, pageContext }: Props) => {
         fluid={featuredImgFluid}
       />}
       {featuredImgDescription &&
-        <div>yello</div>
-        // <p style={{
-        //   // fontSize: rhythm
-        //   marginBottom: rhythm(1),
-        //   display: `flex`,
-        //   fontWeight: `bold`,
-        //   justifyContent: `center`
-        // }}>
-        //   <div>{featuredImgDescription}</div>
-        //   <a
-        //     style={{
-        //       marginLeft: rhythm(.2)
-        //     }}
-        //     href={photographerLink}>{photographerName}</a>
-        // </p>
+        <p style={{
+          // fontSize: rhythm
+          marginBottom: rhythm(1),
+          display: `flex`,
+          fontWeight: `bold`,
+          justifyContent: `center`
+        }}>yello
+          {/* <div>{featuredImgDescription}</div>
+          <a
+            style={{
+              marginLeft: rhythm(.2)
+            }}
+            href={photographerLink}>{photographerName}</a> */}
+        </p>
       }
 
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
